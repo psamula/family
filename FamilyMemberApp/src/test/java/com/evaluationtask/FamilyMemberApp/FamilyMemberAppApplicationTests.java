8576d67fe681dd41ed65cc4bc82e8d1f296c0dc3
Pawel Samula <=>
2023-02-12 13:02:40 +0100

    first commit

---
+++ FamilyMemberApp/src/test/java/com/evaluationtask/FamilyMemberApp/FamilyMemberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluationtask.FamilyMemberApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMemberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
